b2ad50eec6274888ded980003e00fe2d16a9ced6
itz-Hiru <hirumithagithub@gmail.com>
2025-04-09 15:51:00 +0530

    Created delete income API

--- controllers/income.controller.js
+++ controllers/income.controller.js
@@ -1,41 +1,56 @@
-const User = require("../models/User.model")
-const Income = require("../models/Income.model")
+const User = require("../models/User.model");
+const Income = require("../models/Income.model");
 
 exports.addIncome = async (req, res) => {
-    const userId = req.user.id;
+  const userId = req.user.id;
 
-    try {
-        const { icon, source, amount, date } = req.body;
+  try {
+    const { icon, source, amount, date } = req.body;
 
-        if (!source || !amount || !date) {
-            return res.status(404).json({ message: "All fields are required to add new income!" });
-        }
-        const newIncome = new Income ({
-            userId,
-            icon,
-            source,
-            amount,
-            date: new Date(date)
-        });
-
-        await newIncome.save();
-        res.status(200).json(newIncome);
-
-    } catch (e) {
-        res.status(500).json({ message: "Error while adding income ", error: e.message });
+    if (!source || !amount || !date) {
+      return res
+        .status(404)
+        .json({ message: "All fields are required to add new income!" });
     }
+    const newIncome = new Income({
+      userId,
+      icon,
+      source,
+      amount,
+      date: new Date(date),
+    });
+
+    await newIncome.save();
+    res.status(200).json(newIncome);
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: "Error while adding income ", error: e.message });
+  }
 };
 
 exports.getAllIncomes = async (req, res) => {
-    const userId = req.user.id;
+  const userId = req.user.id;
 
-    try {
-        const income = await Income.find({ userId }).sort({ date: -1 });
-        res.status(200).json(income);
-    } catch (e) {
-        res.status(500).json({ message: "Error while getting incomes ", error: e.message});
-    }
+  try {
+    const income = await Income.find({ userId }).sort({ date: -1 });
+    res.status(200).json(income);
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: "Error while getting incomes ", error: e.message });
+  }
 };
 
-exports.deleteIncome = async (req, res) => {};
+exports.deleteIncome = async (req, res) => {
+  try {
+    await Income.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Income deleted successfully" });
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: "Error while deleting income ", error: e.message });
+  }
+};
+
 exports.downloadIncomesExcel = async (req, res) => {};
